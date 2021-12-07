faadd20b4ddd72e471241ac015a2769a62c7c86d
T U <tylerletendre98@gmail.com>
2021-12-07 17:58:25 -0500

    got new progress bars and now can change the active value of the bars

--- beyond40task/src/App.jsx
+++ beyond40task/src/App.jsx
@@ -1,30 +1,28 @@
-import Header from './components/header/Header';
-import ReviewsAverage from './components/reviewsAverage/ReviewsAverage'
-import WriteReviewButton from './components/writeReviewButton/WriteReviewButton';
-import AskQuestionButton from './components/AskQuestion/AskQuestionButton';
-import ProgressBars from './components/progessbars/ProgressBars';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-
-
-
+import Header from "./components/header/Header";
+import ReviewsAverage from "./components/reviewsAverage/ReviewsAverage";
+import WriteReviewButton from "./components/writeReviewButton/WriteReviewButton";
+import AskQuestionButton from "./components/AskQuestion/AskQuestionButton";
+import ProgressBars from "./components/progessbars/ProgressBars";
+import ProgressBar from "react-bootstrap/esm/ProgressBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="header"> 
-        <Header/>
+      <div className="header">
+        <Header />
       </div>
       <div className="body">
         <div>
-          <ReviewsAverage/> 
+          <ReviewsAverage />
         </div>
         <div>
-          <ProgressBars/>
+          <ProgressBars />
         </div>
         <div className="review-button">
-          <AskQuestionButton/>
-          <WriteReviewButton/>
+          <AskQuestionButton />
+          <WriteReviewButton />
         </div>
       </div>
     </div>
--- beyond40task/src/components/progessbars/ProgressBars.jsx
+++ beyond40task/src/components/progessbars/ProgressBars.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Star from "@material-ui/icons/Star";
 import { StarOutline } from "@material-ui/icons";
+import ProgressBar from "react-bootstrap/ProgressBar";
 import "./progressBars.css";
 
 export default function ProgressBars() {
@@ -14,15 +15,8 @@ export default function ProgressBars() {
           <Star style={{ fontSize: "16px", color: "#777" }} />
           <Star style={{ fontSize: "16px", color: "#777" }} />
         </label>
-        <div class="progress">
-          <div
-            class="progress-bar"
-            role="progressbar"
-            aria-valuenow="75"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            style={{ width: "130px", backgroundColor: "#E89F0E" }}
-          ></div>
+        <div>
+          <ProgressBar now={87} variant="warning" style={{ width: "130px" }} />
         </div>
         <div className="outside-label">
           <p>(2940)</p>
@@ -36,18 +30,11 @@ export default function ProgressBars() {
           <Star style={{ fontSize: "16px", color: "#777" }} />
           <StarOutline style={{ fontSize: "16px", color: "#777" }} />
         </label>
-        <div class="progress">
-          <div
-            class="progress-bar"
-            role="progressbar"
-            aria-valuenow="75"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            style={{ width: "130px", backgroundColor: "#E89F0E" }}
-          ></div>
+        <div>
+          <ProgressBar now={20} variant="warning" style={{ width: "130px" }} />
         </div>
         <div className="outside-label">
-          <p>(2940)</p>
+          <p>(230)</p>
         </div>
       </div>
       <div className="progress-bars">
@@ -58,18 +45,11 @@ export default function ProgressBars() {
           <StarOutline style={{ fontSize: "16px", color: "#777" }} />
           <StarOutline style={{ fontSize: "16px", color: "#777" }} />
         </label>
-        <div class="progress">
-          <div
-            class="progress-bar"
-            role="progressbar"
-            aria-valuenow="50"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            style={{ width: "130px", backgroundColor: "#E89F0E" }}
-          ></div>
+        <div>
+          <ProgressBar now={3} variant="warning" style={{ width: "130px" }} />
         </div>
         <div className="outside-label">
-          <p>(2940)</p>
+          <p>(50)</p>
         </div>
       </div>
       <div className="progress-bars">
@@ -80,18 +60,11 @@ export default function ProgressBars() {
           <StarOutline style={{ fontSize: "16px", color: "#777" }} />
           <StarOutline style={{ fontSize: "16px", color: "#777" }} />
         </label>
-        <div class="progress">
-          <div
-            class="progress-bar"
-            role="progressbar"
-            aria-valuenow="75"
-            aria-valuemin="0"
-            aria-valuemax="50"
-            style={{ width: "130px", backgroundColor: "#E89F0E" }}
-          ></div>
+        <div>
+          <ProgressBar now={10} variant="warning" style={{ width: "130px" }} />
         </div>
         <div className="outside-label">
-          <p>(2940)</p>
+          <p>(5)</p>
         </div>
       </div>
       <div className="progress-bars">
@@ -102,18 +75,11 @@ export default function ProgressBars() {
           <StarOutline style={{ fontSize: "16px", color: "#777" }} />
           <StarOutline style={{ fontSize: "16px", color: "#777" }} />
         </label>
-        <div class="progress">
-          <div
-            class="progress-bar"
-            role="progressbar"
-            aria-valuenow="75"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            style={{ width: "130px", backgroundColor: "#E89F0E" }}
-          ></div>
+        <div>
+          <ProgressBar now={9} variant="warning" style={{ width: "130px" }} />
         </div>
         <div className="outside-label">
-          <p>(2940)</p>
+          <p>(7)</p>
         </div>
       </div>
     </div>
